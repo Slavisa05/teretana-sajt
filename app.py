52a456ae32138a9adc794f59c50c10b566aa9fb5
Slavisa05 <slavisaarsenijevic05@gmail.com>
2025-08-30 20:35:26 +0200

    testiram deploy

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ CORS(app)
 def root():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
